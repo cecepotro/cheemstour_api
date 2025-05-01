ab2fc2e219efc9703b369f127a9d0236eeef260c
jcecena <jantonio.cecena@gmail.com>
2025-05-01 16:15:14 -0700

    Se agregaron parámetros de latitud y longitud

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from entities.trip import Trip
 
 app = Flask(__name__)
@@ -7,6 +7,15 @@ app = Flask(__name__)
 def trips():
     trips = Trip.get()
     return trips
+
+@app.route('/trip', methods=['POST'])
+def save_trip():
+    data = request.json
+    trip = Trip(name=data['name'], city = data['city'], country=data['country'], latitude=data['latitude'], longitude=data['longitude'])
+    id = Trip.save(trip)
+    success = id is not None
+    return jsonify(success), 201
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- entities/trip.py
+++ entities/trip.py
@@ -3,18 +3,35 @@ from mysql.connector import Error
 
 class Trip:
 
-    def __init__(self, name, city, country):
+    def __init__(self, name, city, country, latitude, longitude):
         self.name = name
         self.city = city
         self.country = country
+        self.latitude = latitude
+        self.longitude = longitude
 
     @classmethod
     def get(cls):
         try:
             connection = get_connection()
             cursor = connection.cursor(dictionary = True)
-            cursor.execute('SELECT id, name, city, country FROM trip')
+            cursor.execute('SELECT id, name, city, country, latitude, longitude FROM trip')
             return cursor.fetchall()
+        except Error as ex:
+            return str(ex)
+        finally:
+            cursor.close()
+            connection.close()
+    
+    @classmethod
+    def save(cls, trip):
+        try:
+            connection = get_connection()
+            cursor = connection.cursor()
+            cursor.execute('INSERT INTO trip (name, city, country, latitude, longitude) VALUES (%s, %s, %s, %s, %s)',
+                           (trip.name, trip.city, trip.country, trip.latitude, trip.longitude))
+            connection.commit()
+            return cursor.lastrowid
         except Error as ex:
             return str(ex)
         finally:
